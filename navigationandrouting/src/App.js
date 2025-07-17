4b2fb83dbd67feac3ca8a80adfe24e82c096a3f8
Abhishek Srivastava <avix1011.a@gmail.com>
2025-07-17 17:50:41 +0530

    Navigation and Routing
    Updated components, and used Accordino

--- navigationandrouting/src/App.js
+++ navigationandrouting/src/App.js
@@ -1,80 +1,28 @@
-import {
-  GoBell,
-  GoRocket,
-  GoAlertFill,
-  GoShieldCheck,
-  GoEyeClosed,
-} from 'react-icons/go';
-import Button from './Button';
+import Accordion from './components/Accordion';
 
 function App() {
-  const handleClick = () => {
-    console.log('Clicked or Hovered!');
-  };
+  const items = [
+    {
+      id: 'l2kj5',
+      label: 'Can I use React on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      id: 'lk2j35lkj',
+      label: 'Can I use Javascript on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      id: 'l1kj2i0g',
+      label: 'Can I use CSS on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+  ];
 
-  return (
-    <div className="space-y-4 p-6 bg-gray-100 min-h-screen">
-      <div>
-        <Button
-          secondary
-          outline
-          rounded
-          onClick={handleClick}
-          className="hover:bg-gray-200 hover:scale-105 transition-transform duration-200"
-        >
-          <GoBell className="mr-2" />
-          Notifications
-        </Button>
-      </div>
-
-      <div>
-        <Button
-          danger
-          outline
-          onMouseEnter={handleClick}
-          className="hover:bg-red-100 hover:text-red-700 hover:scale-105 transition duration-200"
-        >
-          <GoAlertFill className="mr-2" />
-          Report Issue
-        </Button>
-      </div>
-
-      <div>
-        <Button
-          warning
-          onMouseLeave={handleClick}
-          className="hover:bg-yellow-300 hover:scale-110 transition duration-200"
-        >
-          <GoRocket className="mr-2" />
-          Launch Offer
-        </Button>
-      </div>
-
-      <div>
-        <Button
-          success
-          outline
-          rounded
-          className="hover:bg-green-100 hover:text-green-700 transition duration-200"
-        >
-          <GoShieldCheck className="mr-2" />
-          Secure Access
-        </Button>
-      </div>
-
-      <div>
-        <Button
-          primary
-          rounded
-          onMouseOver={handleClick}
-          className="hover:bg-blue-600 hover:shadow-lg transition-transform duration-300"
-        >
-          <GoEyeClosed className="mr-2" />
-          Hide Content
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordion items={items} />;
 }
 
 export default App;
--- navigationandrouting/src/index.js
+++ navigationandrouting/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el);
+const element = document.getElementById('root');
+const root = ReactDOM.createRoot(element);
 
 root.render(<App />);
